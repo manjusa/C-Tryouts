413033d209950f843a0a87cbcad26fd260ab530c
manjusa <thizismanju@mail.com>
2024-11-05 08:40:15 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using C_11PdfBookTryOuts.ch5;
+
+Console.WriteLine("Hello, World!");
+ch5MethodsTryouts();
+
+void ch5MethodsTryouts()
+{
+    // Create two PersonCh5 objects
+    PersonCh5 person1 = new PersonCh5("John Doe", "Mars");
+    PersonCh5 person2 = new PersonCh5("Jane Doe", "Venus");
+
+    // Use the static Marry method to marry person1 and person2
+    PersonCh5.Marry(person1, person2);
+
+    // Use the instance Marry method to verify the marriage
+    person1.Marry(person2);
+
+    // Print the marital status and spouse details
+    Console.WriteLine($"Person1: {person1.Name}, Married: {person1.Married}, Spouse: {(person1.Spouse?.Name ?? "None")}");
+    Console.WriteLine($"Person2: {person2.Name}, Married: {person2.Married}, Spouse: {(person2.Spouse?.Name ?? "None")}");
+}
---
+++ ch5/PersonCh5.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace C_11PdfBookTryOuts.ch5
+{
+    public class PersonCh5 : object
+    {
+        //Author's class is much bigger, mine in concsie for this "More about methods"ch05
+        //     eg https://github.com/markjprice/cs11dotnet7/blob/main/notebooks/Chapter05.dib
+
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<PersonCh5> Children = new();
+
+        // constants
+        public const string Species = "Homo Sapien";
+
+        // read-only fields
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+
+        //of interest properties
+        private bool married = false;
+        public bool Married => married;
+
+        private PersonCh5? spouse = null;
+        public PersonCh5? Spouse => spouse;
+
+        // constructors
+        public PersonCh5()
+        {
+            // set default values for fields
+            // including read-only fields
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public PersonCh5(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+      
+        // static method to marry
+        public static void Marry(PersonCh5 p1, PersonCh5 p2)
+        {
+            p1.Marry(p2);
+        }
+        // instance method to marry
+        public void Marry(PersonCh5 partner)
+        {
+            if (married) return;
+            spouse = partner;
+            married = true;
+            partner.Marry(this); // this is the current object
+        }
+    }
+}
